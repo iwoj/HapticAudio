ea7dacad36dfa24e068402434731cf7b356f9374
Ian Wojtowicz <iwojtowicz@aldrichpears.com>
2019-12-03 12:16:30 -0800

    Now finds new devices and sorts them by signal strength.

--- Source Code/TouchMap/TouchMap.ino
+++ Source Code/TouchMap/TouchMap.ino
@@ -1,6 +1,14 @@
+// =========================
+// Must upload with No OTA (Large APP) partition scheme
+
+// General stuff
+#include <ArduinoSort.h>
+
+
+
 // WiFi Stuff ---------
-#include <WiFi.h>         
-#include <WebServer.h>    
+#include <WiFi.h>
+#include <WebServer.h>
 #include <AutoConnect.h>
 WebServer   Server;
 AutoConnect Portal(Server);
@@ -12,39 +20,47 @@ AutoConnect Portal(Server);
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
-#include <LinkedList.h>
 
-#define PROXIMITY_LIMIT_RSSI -55
-int scanTime = 1; //In seconds
+#define PROXIMITY_LIMIT_RSSI  -55
+#define MAX_CLOSE_DEVICES     10
+byte scanTime = 1; //In seconds
 BLEScan* pBLEScan;
 bool runningScan = false;
-LinkedList<BLEAdvertisedDevice> closeDevices;
-LinkedList<BLEAdvertisedDevice> previousCloseDevices;
-
+byte deviceCounter = 0;
+BLEAdvertisedDevice closeDevices[MAX_CLOSE_DEVICES];
+BLEAdvertisedDevice previousCloseDevices[MAX_CLOSE_DEVICES];
+BLEAdvertisedDevice nullDevice = BLEAdvertisedDevice();
 
 
 // Capacitive Touch Stuff ---------
 const int TOUCH_SENSOR_THRESHOLD = 20;
-int touchSensor1Value=0;
-int touchSensor2Value=0;
-int touchSensor3Value=0;
+int touchSensor1Value = 0;
+int touchSensor2Value = 0;
+int touchSensor3Value = 0;
 boolean touch1Start = false;
 boolean touch2Start = false;
 boolean touch3Start = false;
 
 
 
-const int LED_PIN = 5; // Thing's onboard LED
+const byte LED_PIN = 5; // Thing's onboard LED
+
+
+
+#define DEBUG     false
 
 
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
-      if (advertisedDevice.haveRSSI()){
+      if (advertisedDevice.haveRSSI()) {
         if ((int)advertisedDevice.getRSSI() > PROXIMITY_LIMIT_RSSI) {
-//          Serial.printf("Advertised Device: %s", advertisedDevice.toString().c_str());
-//          Serial.printf(", Rssi: %d \n", (int)advertisedDevice.getRSSI());
-          closeDevices.add(advertisedDevice);
+          //          Serial.printf("Advertised Device: %s", advertisedDevice.toString().c_str());
+          //          Serial.printf(", Rssi: %d \n", (int)advertisedDevice.getRSSI());
+          if (deviceCounter < MAX_CLOSE_DEVICES) {
+            closeDevices[deviceCounter] = advertisedDevice;
+            deviceCounter++;
+          }
         }
       }
     }
@@ -54,9 +70,11 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_PIN,OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   connectWiFi();
   setupBLE();
+  clearDevices(previousCloseDevices);
+  clearDevices(closeDevices);
   runBLEScan();
 }
 
@@ -81,7 +99,7 @@ void connectWiFi()
 {
   Server.on("/", rootPage);
   if (Portal.begin()) {
-    Serial.println("WiFi connected: " + WiFi.localIP().toString());
+    if (DEBUG) Serial.println("WiFi connected: " + WiFi.localIP().toString());
   }
 }
 
@@ -97,122 +115,162 @@ void setupBLE() {
 
 
 void runBLEScan() {
-  Serial.println("Scanning BLE...");
-//  clearDevices(previousCloseDevices);
-  previousCloseDevices = closeDevices;
-//  clearDevices(closeDevices);
-  closeDevices = LinkedList<BLEAdvertisedDevice>();
+  if (DEBUG) Serial.println("Scanning BLE...");
+  clearDevices(previousCloseDevices);
+  bleadCopy(closeDevices, previousCloseDevices, MAX_CLOSE_DEVICES);
+  clearDevices(closeDevices);
+  deviceCounter = 0;
   runningScan = true;
   pBLEScan->start(scanTime, scanComplete);
 }
 
 
 
-void scanComplete(BLEScanResults foundDevices) {  
+void scanComplete(BLEScanResults foundDevices) {
   runningScan = false;
-  closeDevices.sort(sortDevices);
+  if (DEBUG) sortDevices(closeDevices, deviceSortByRSSI, MAX_CLOSE_DEVICES);
+  if (DEBUG) Serial.println("closeDevices:");
+  if (DEBUG) printDeviceList(closeDevices, MAX_CLOSE_DEVICES);
+  if (DEBUG) Serial.println("previousCloseDevices:");
+  if (DEBUG) printDeviceList(previousCloseDevices, MAX_CLOSE_DEVICES);
   handleBLEProximity();
   pBLEScan->clearResults();
-  Serial.println("");
+  if (DEBUG) Serial.println("");
   runBLEScan();
 }
 
 
 
-LinkedList<BLEAdvertisedDevice> listDiff(LinkedList<BLEAdvertisedDevice> a, LinkedList<BLEAdvertisedDevice> b) {
-  LinkedList<BLEAdvertisedDevice> combined;
-  LinkedList<int> toRemove;
-  for (int i = 0; i < a.size(); i++) {
-    combined.add(a.get(i));
+BLEAdvertisedDevice *arrSubtract(BLEAdvertisedDevice a[], BLEAdvertisedDevice b[]) {
+  if (DEBUG) Serial.println("Got to arrSubtract");
+  BLEAdvertisedDevice combined[MAX_CLOSE_DEVICES*2];
+  BLEAdvertisedDevice duplicates[MAX_CLOSE_DEVICES];
+  byte duplicatesIndex = 0;
+  static BLEAdvertisedDevice differences[MAX_CLOSE_DEVICES];
+  byte differencesIndex = 0;
+  
+  clearDevices(duplicates);
+  clearDevices(differences);
+  
+  if (DEBUG) Serial.println("b:");
+  if (DEBUG) printDeviceList(b, MAX_CLOSE_DEVICES);
+  
+  // Combine a + b
+  for (byte i = 0; i < MAX_CLOSE_DEVICES; i++) {
+    combined[i] = a[i];
   }
-  for (int i = 0; i < b.size(); i++) {
-    combined.add(b.get(i));
+  for (byte i = 0; i < MAX_CLOSE_DEVICES; i++) {
+    combined[MAX_CLOSE_DEVICES+i] = b[i];
   }
-  combined.sort(sortDevices);
-//  for (int i = 0; i < combined.size(); i++) {
-//    if (i >= combined.size() - 1 && 
-//        combined.get(i).getAddress().toString() == combined.get(i+1).getAddress().toString()) {
-//      toRemove.add(i);
-//      toRemove.add(i+1);
-//    }
-//  }
-//  toRemove.sort(sortIntReverse);
-//  for (int i = 0; i < toRemove.size(); i++) {
-//    combined.remove(toRemove.get(i));
-//  }
-  return combined;
+  if (DEBUG) Serial.println("Combined a + b");
+  
+  sortDevices(combined, deviceSortByAddress, MAX_CLOSE_DEVICES*2);
+  
+  // Find duplicates
+  for (byte i = 0; i < MAX_CLOSE_DEVICES*2 - 1; i++) {
+    if (combined[i].getAddress().toString() == combined[i+1].getAddress().toString()) {
+      duplicates[duplicatesIndex] = combined[i];
+      duplicatesIndex++;
+      i++;
+    }
+  }
+  if (DEBUG) Serial.printf("Searched for duplicates. Found %d:\n", deviceCount(duplicates));
+  if (DEBUG) printDeviceList(duplicates, MAX_CLOSE_DEVICES);
+  
+  // Perform subtraction by storing non-duplicates
+  boolean isDuplicate;
+  for (byte i = 0; i < MAX_CLOSE_DEVICES; i++) {
+    if (a[i].getAddress().toString() == nullDevice.getAddress().toString()) continue; // Ignore nullDevices
+    isDuplicate = false;
+    for (byte j = 0; j < MAX_CLOSE_DEVICES; j++) {
+      if (duplicates[j].getAddress().toString() == nullDevice.getAddress().toString()) continue; // Ignore nullDevices
+      if (a[i].getAddress().toString() == duplicates[j].getAddress().toString()) {
+        if (DEBUG) Serial.println("Duplicate found.");
+        isDuplicate = true;
+      }
+    }
+    if (!isDuplicate) {
+      differences[differencesIndex] = a[i];
+      differencesIndex++;
+    }
+  }
+  if (DEBUG) Serial.println("Performed subtraction");
+  
+  return differences;
 }
 
 
 
-LinkedList<BLEAdvertisedDevice> listNotIn(LinkedList<BLEAdvertisedDevice> a, LinkedList<BLEAdvertisedDevice> b) {
-  LinkedList<BLEAdvertisedDevice> diffList = listDiff(a, b);
-  return listDiff(diffList, a);
-}
-
-
-
-
-
-void printDeviceList(LinkedList<BLEAdvertisedDevice> a) {
-  for (int i = 0; i < a.size() - 1; i++) {
-    Serial.print(a.get(i).getAddress().toString().c_str());
-    Serial.print(", ");
+void printDeviceList(BLEAdvertisedDevice a[], byte arrLen) {
+  byte maxDevices = deviceCount(a);
+  byte deviceCounter = 0;
+  for (byte i = 0; i < arrLen - 1; i++) {
+    if (a[i].getAddress().toString() != nullDevice.getAddress().toString()) {
+      deviceCounter++;
+      Serial.print(a[i].getAddress().toString().c_str());
+      if (deviceCounter != maxDevices) Serial.print(", ");
+    }
   }
-  Serial.print(a.get(a.size() - 1).getAddress().toString().c_str());
+  if (arrLen > 0 && a[arrLen - 1].getAddress().toString() != nullDevice.getAddress().toString()) {
+    Serial.print(a[arrLen - 1].getAddress().toString().c_str());
+  }
+  Serial.println("");
 }
-
 
 
 
 void handleBLEProximity() {
   if (!runningScan) {
-    if (closeDevices.size() > 0) {
-      Serial.println(closeDevices.size() + (String)" device(s) close by!");
-      // Do stuff.
-    }
-    else if (closeDevices.size() == 0) {
+    byte numCloseDevices = deviceCount(closeDevices);
+    
+    if (numCloseDevices == 0) {
       Serial.println("No devices close by.");
-      // Do stuff.
+      return;
     }
-//    LinkedList<BLEAdvertisedDevice> ld = listDiff(closeDevices, previousCloseDevices);
-//    printDeviceList(ld);
-//    ld.clear();
-    printDeviceList(listDiff(closeDevices, previousCloseDevices));
-    Serial.print("\n");
+    
+    sortDevices(closeDevices, deviceSortByRSSI, MAX_CLOSE_DEVICES);
+    Serial.println(deviceCount(closeDevices) + (String)" device(s) close by.");
+    Serial.printf("Closest device: %s\n", closeDevices[0].getAddress().toString().c_str());
+    
+    BLEAdvertisedDevice *newDevices = arrSubtract(closeDevices, previousCloseDevices);
+    Serial.print("Number of new devices: ");
+    Serial.println(deviceCount(newDevices));
+    if (DEBUG) Serial.print("New devices: ");
+    if (DEBUG) printDeviceList(newDevices, MAX_CLOSE_DEVICES);
+    if (DEBUG) Serial.print("\n");
     
     // Send insert/remove commands to Meteor
-//    LinkedList<BLEAdvertisedDevice> newDevices = listNotIn(closeDevices, previousCloseDevices);
-//    Serial.print("New devices: ");
-//    printDeviceList(newDevices);
-//    Serial.print("\n");
+    //    LinkedList<BLEAdvertisedDevice> newDevices = listNotIn(closeDevices, previousCloseDevices);
+    //    Serial.print("New devices: ");
+    //    printDeviceList(newDevices);
+    //    Serial.print("\n");
     
-//    LinkedList<BLEAdvertisedDevice> removedDevices = listNotIn(previousCloseDevices, closeDevices);
-//    Serial.print("Removed devices: ");
-//    printDeviceList(removedDevices);
-//    Serial.print("\n");
+    //    LinkedList<BLEAdvertisedDevice> removedDevices = listNotIn(previousCloseDevices, closeDevices);
+    //    Serial.print("Removed devices: ");
+    //    printDeviceList(removedDevices);
+    //    Serial.print("\n");
   }
 }
 
 
 
 void readSensors() {
-  int numberOfReadings = 10;
-  
+  byte numberOfReadings = 10;
+
   touchSensor1Value = 0;
   touchSensor2Value = 0;
   touchSensor3Value = 0;
-  
-  for(int i=0; i< numberOfReadings; i++)
+
+  for (byte i = 0; i < numberOfReadings; i++)
   {
     touchSensor1Value += touchRead(T0);
     touchSensor2Value += touchRead(T2);
     touchSensor3Value += touchRead(T3);
   }
-  
-  touchSensor1Value = touchSensor1Value/numberOfReadings;
-  touchSensor2Value = touchSensor2Value/numberOfReadings;
-  touchSensor3Value = touchSensor3Value/numberOfReadings;
+
+  touchSensor1Value = touchSensor1Value / numberOfReadings;
+  touchSensor2Value = touchSensor2Value / numberOfReadings;
+  touchSensor3Value = touchSensor3Value / numberOfReadings;
 }
 
 
@@ -228,7 +286,7 @@ void senseTouchEvents() {
     Serial.println("Touch1 end");
     touch1Start = false;
   }
-  
+
   if (touchSensor2Value < TOUCH_SENSOR_THRESHOLD && !touch2Start) {
     Serial.print("Touch2 start (");
     Serial.print(touchSensor2Value);
@@ -239,7 +297,7 @@ void senseTouchEvents() {
     Serial.println("Touch2 end");
     touch2Start = false;
   }
-  
+
   if (touchSensor3Value < TOUCH_SENSOR_THRESHOLD && !touch3Start) {
     Serial.print("Touch3 start (");
     Serial.print(touchSensor3Value);
@@ -254,15 +312,88 @@ void senseTouchEvents() {
 
 
 
-int sortDevices(BLEAdvertisedDevice &a, BLEAdvertisedDevice &b) {
-  return strcmp(a.getAddress().toString().c_str(), 
-                b.getAddress().toString().c_str());
+void sortDevices(BLEAdvertisedDevice devices[], int8_t (*comparator)(BLEAdvertisedDevice &a, BLEAdvertisedDevice &b), byte arrLen) {
+  boolean swapped;
+  BLEAdvertisedDevice temp;
+  do
+  {
+      swapped = false;
+      for (byte i = 0; i < arrLen - 1; i++)
+      {
+          if (DEBUG) Serial.print("Loop ");
+          if (DEBUG) Serial.println(i);
+          if (comparator(devices[i], devices[i+1]) > 0)
+          {
+              temp = devices[i];
+              devices[i] = devices[i + 1];
+              devices[i + 1] = temp;
+              swapped = true;
+          }
+      }
+  } while (swapped);
 }
 
 
 
-int sortIntReverse(int &a, int &b) {
-  return a < b ? 1 : -1;
+int8_t deviceSortByAddress(BLEAdvertisedDevice &a, BLEAdvertisedDevice &b) {
+  int8_t result = 0;
+  if (DEBUG) Serial.print("Comparing ");
+  if (DEBUG) Serial.print(a.getAddress().toString().c_str());
+  if (DEBUG) Serial.print(" to ");
+  if (DEBUG) Serial.print(b.getAddress().toString().c_str());
+  if (DEBUG) Serial.print(": ");
+  if (b.getAddress().toString() == "00:00:00:00:00:00") result = -1; // Keep empty devices at the end of list.
+  else if (a.getAddress().toString() == "00:00:00:00:00:00") result = 1; // Keep empty devices at the end of list.
+  else result = strcmp(a.getAddress().toString().c_str(),
+                      b.getAddress().toString().c_str());
+  if (DEBUG) Serial.println(result);
+  return result;
+}
+
+int8_t deviceSortByRSSI(BLEAdvertisedDevice &a, BLEAdvertisedDevice &b) {
+  int8_t result = 0;
+  if (DEBUG) Serial.print("Comparing ");
+  if (DEBUG) Serial.print(a.getAddress().toString().c_str());
+  if (DEBUG) Serial.print(" (");
+  if (DEBUG) Serial.print(a.getRSSI());
+  if (DEBUG) Serial.print(") to ");
+  if (DEBUG) Serial.print(b.getAddress().toString().c_str());
+  if (DEBUG) Serial.print(" (");
+  if (DEBUG) Serial.print(b.getRSSI());
+  if (DEBUG) Serial.print("): ");
+  
+  if (a.getRSSI() > b.getRSSI()) result = -1;
+  else if (a.getRSSI() < b.getRSSI()) result = 1;
+  
+  if (DEBUG) Serial.println(result);
+  return result;
 }
 
 
+
+void clearDevices(BLEAdvertisedDevice devices[]) {
+  for (byte i = 0; i < MAX_CLOSE_DEVICES; i++) {
+    if (DEBUG) Serial.printf("Clearing device %d\n", (int)i);
+    devices[i] = nullDevice;
+  }
+}
+
+
+
+byte deviceCount(BLEAdvertisedDevice devices[]) {
+  byte count = 0;
+  for (byte i = 0; i < MAX_CLOSE_DEVICES; i++) {
+    if (devices[i].getAddress().toString() != nullDevice.getAddress().toString())
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+
+void bleadCopy(BLEAdvertisedDevice arrayOriginal[], BLEAdvertisedDevice arrayCopy[], byte arraySize){ //Copy function
+  for(byte i=0; i<arraySize; i++){
+    arrayCopy[i]=arrayOriginal[i];  
+  }
+}
